f4aa1bf4df75dd2a03b32364665aca6d6a398e8a
bitweft <bitweft@gmail.com>
2020-04-18 12:36:09 +0800

    Add ability to enter your name before entering books list

--- app/src/main/java/com/example/bookinformation/HomeActivity.kt
+++ app/src/main/java/com/example/bookinformation/HomeActivity.kt
@@ -1,12 +1,32 @@
 package com.example.bookinformation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 
 class HomeActivity : AppCompatActivity() {
+    lateinit var enterButton: Button
+    lateinit var userName: EditText
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        userName = findViewById(R.id.user_name)
+        enterButton = findViewById(R.id.enter_books_button)
+
+        userName.addTextChangedListener { enableButton() }
+        enterButton.setOnClickListener { enterBooksList() }
+    }
+
+    private fun enableButton() {
+        enterButton.isEnabled = userName.text.trim().isNotEmpty()
+    }
+
+    private fun enterBooksList() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
