02b2f1a4e96968f7b2718f6621f9d5c2bf28719e
bitweft <bitweft@gmail.com>
2020-04-19 17:29:29 +0800

    Check if author_name exists in response before using it

--- app/src/main/java/com/bookinformation/models/Book.kt
+++ app/src/main/java/com/bookinformation/models/Book.kt
@@ -9,12 +9,11 @@ data class Book(val title: String, val coverUrl: String, val author: String, val
     companion object {
         private const val coverEditionKey = "cover_edition_key"
         private const val editionKey = "edition_key"
-        private var bookId = "OL22620224M"
 
         fun getBook(book: JSONObject): Book {
+            val bookId = getBookId(book)
             val title = book.getString("title")
             val author = getAuthor(book)
-            bookId = getBookId(book)
             val coverUrl = getCoverUrl(bookId)
             return Book(title, coverUrl, author, bookId)
         }
@@ -22,25 +21,26 @@ data class Book(val title: String, val coverUrl: String, val author: String, val
         private fun getBookId(book: JSONObject): String {
             if (book.has(coverEditionKey)) {
                 return book.getString(coverEditionKey)
-            } else if (book.has(editionKey)) {
+            } else {
                 val ids = book.getJSONArray(editionKey)
                 return ids.getString(0)
             }
-            return bookId
         }
 
         private fun getAuthor(book: JSONObject): String {
             val authorNames: MutableList<String> = ArrayList()
-
-            val authors = book.getJSONArray("author_name")
-            for (i in 0 until authors.length()) {
-                authorNames.add(authors.getString(i))
+            val authorNameKey = "author_name"
+            if (book.has(authorNameKey)) {
+                val authors = book.getJSONArray(authorNameKey)
+                for (i in 0 until authors.length()) {
+                    authorNames.add(authors.getString(i))
+                }
             }
             return TextUtils.join(", ", authorNames)
         }
 
-        private fun getCoverUrl(coverId: String): String {
-            return "http://covers.openlibrary.org/b/olid/$coverId-M.jpg?default=false"
+        private fun getCoverUrl(bookId: String): String {
+            return "http://covers.openlibrary.org/b/olid/$bookId-M.jpg?default=false"
         }
     }
 }
