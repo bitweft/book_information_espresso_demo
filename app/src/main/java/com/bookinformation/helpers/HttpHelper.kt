a90920e38318c543f74e8e72a27b1bb73b0dc526
bitweft <bitweft@gmail.com>
2020-04-23 17:20:39 +0800

    Add log to see the URL called

--- app/src/main/java/com/bookinformation/helpers/HttpHelper.kt
+++ app/src/main/java/com/bookinformation/helpers/HttpHelper.kt
@@ -1,5 +1,6 @@
 package com.bookinformation.helpers
 
+import android.util.Log
 import okhttp3.Callback
 import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
@@ -7,12 +8,14 @@ import okhttp3.Request
 
 class HttpHelper {
     private val client = OkHttpClient()
+    private val tag = "HttpHelper"
 
     fun getResponse(url: HttpUrl, callback: Callback) {
         val request = Request.Builder()
             .url(url)
             .build()
 
+        Log.i(tag, "URL = $url")
         client.newCall(request).enqueue(callback)
     }
 
